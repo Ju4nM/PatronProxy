f6b363b3559db7c6685f50dea785bdab52e2e2e2
Ju4nM <jenriquemflores@gmail.com>
2023-11-21 07:56:37 -0700

    Primer commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿
+using PatronProxy.Proxy;
+
+APIProxy proxy = new APIProxy();
+
+proxy.UserName = "admin";
+
+string result = proxy.Request("/home");
+
+Console.WriteLine(result);
---
+++ Service/API.cs
@@ -0,0 +1,15 @@
+﻿using PatronProxy.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatronProxy.Service {
+    public class API: IAPI {
+
+        public string Request (string route) {
+            return $"{route} - Status Code 200";
+        }
+    }
+}
